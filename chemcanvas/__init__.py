29989e557a2882d1abce7fafa58024cf91c03c0a
Arindam Chaudhuri <ksharindam@gmail.com>
2023-10-20 10:01:11 +0530

    0.1.1 release

--- chemcanvas/__init__.py
+++ chemcanvas/__init__.py
@@ -31,7 +31,7 @@ Description = A Chemical Drawing program
 #
 
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 COPYRIGHT_YEAR = "2022-2023"
 AUTHOR_NAME = "Arindam Chaudhuri"
 AUTHOR_EMAIL = "arindamsoft94@gmail.com"
