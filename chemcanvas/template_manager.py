edbb7eb3f4a4312d876b60f911ef23f80cd263cb
Arindam Chaudhuri <ksharindam@gmail.com>
2025-06-15 11:49:22 +0530

    add crown ethers templates

--- chemcanvas/template_manager.py
+++ chemcanvas/template_manager.py
@@ -32,7 +32,7 @@ def find_template_icon(icon_name):
 
 class TemplateManager:
     # molecule categories
-    categories = ["Amino Acids", "Aromatics", "Bicyclics", "Bridged Polycyclics", "Heterocycles", "Nucleobases", "Rings", "Sugars", "Others"]
+    categories = ["Amino Acids", "Aromatics", "Bicyclics", "Bridged Polycyclics", "Crown Ethers", "Heterocycles", "Nucleobases", "Rings", "Sugars", "Others"]
 
     def __init__(self):
         # dict key is in "name index" format. eg - "cyclohexane", "cyclohexane 1".
